b8266907454dd3c5e4b4cc406aa7b882edf79941
Glauber <glauber_g3@yahoo.com.br>
2016-12-10 02:49:47 +0000

    basic v01

---
+++ Basic/ArquivoTxt.py
@@ -0,0 +1,28 @@
+import sys
+
+name = ' '
+age  = ' '
+hobby = ' '
+
+op = '1'
+
+while op == '1':
+    name = input("What's your name? ")
+    age  = input("What's your age? ")
+    hobby = input("What's your hobby? ")
+    print("\n\nIt's all right?")
+    print("-----------------")
+    print("Name:\t",name)
+    print("age:\t",age)
+    print("mother:\t",hobby)
+    print("-----------------")
+
+    file = open("peaple.db","a")
+    file.write(name)
+    file.write(' ')
+    file.write(age)
+    file.write(' ')
+    file.write(hobby)
+    file.write('\n')
+    op = input("1 - cadastrar ")
+file.close()
