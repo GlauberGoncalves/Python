937ce817c487c1e92226759513f845ea79b2d6ae
Glauber <glauber_g3@yahoo.com.br>
2016-12-11 20:53:51 +0000

    Basic com mais 5 exemplos

---
+++ 05-operadores-logicos.py
@@ -0,0 +1,23 @@
+#operadores Lógicos
+
+# and - e
+# or  - ou
+# not - não
+
+idade = 28
+print('Idade == 28\n')
+print('(idade > 18 and idade < 30)')
+print(idade > 18 and idade < 30)
+print('\n')
+
+print('(idade > 18 and idade == 30)')
+print(idade > 18 and idade == 30)
+print('\n')
+
+print('(idade > 18 or idade < 30)')
+print(idade > 18 or idade == 30)
+print('\n')
+
+print('(idade < 18 or not idade)')
+print(idade < 18 or not idade)
+print('\n')
