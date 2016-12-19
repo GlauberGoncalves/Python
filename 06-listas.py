75f1081f6da3177a73d2cd27568b612c6ea63304
Glauber <glauber_g3@yahoo.com.br>
2016-12-19 19:25:20 +0000

    listas A

---
+++ 06-listas.py
@@ -0,0 +1,14 @@
+# listas
+
+# lista de inteiros
+anos = [1988,1989,1999,2010,2016,2017]
+# lista de strings
+nomes = ['joão','amanda','lucia','patricia','marcela','marcia']
+
+# verificar se existe item na lista
+print('amanda' in nomes)
+
+# tirando item da lista
+print('Lista nomes contem: %d ' %len(nomes))
+nomes.remove('amanda')
+print('Lista nomes contem: %d ' %len(nomes))
