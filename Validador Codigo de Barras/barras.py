9519c74f633e697a16cc5ce674766736cc8ae89a
Glauber <glauber_g3@yahoo.com.br>
2016-12-21 12:53:20 +0000

    função verifica_codigo atualizada

--- Validador Codigo de Barras/barras.py
+++ Validador Codigo de Barras/barras.py
@@ -14,21 +14,22 @@ def gerar_validador(cod):
     if(tamanho%2 == 0):                         # caso o total de numeros do codigo seja par
         for i in range(0,tamanho,2):        
             temp[0] = temp[0]+ int(cod[i])      # soma posições impares
-            temp[1] = temp[1]+ int(cod[i+1])    # soma posições pares
-        print(temp[0])
-        print(temp[1])
-        temp[0] = temp[0]*3 + temp[1]           # aplicação da formula
-        print(temp[0])
+            temp[1] = temp[1]+ int(cod[i+1])    # soma posições pares                
+        temp[0] = temp[1]*3 + temp[0]           # aplicação da formula        
         temp[2] = 10 - (temp[0]%10)             # gerando digito verificador
-        print(temp[2])
+        if(temp[2] == 10):
+            return 0
+        return temp[2]
     else:                                       # caso o total de numeros do codigo seja impar        
         for i in range(0,int(tamanho),2):
             temp[0] = temp[0]+ int(cod[i])  # soma posições impares
             if( i != (tamanho-1)):
                 temp[1] = temp[1] + int(cod[i+1])
         temp[0] = temp[0]*3 + temp[1]   # aplicação da formula
-        temp[2] = 10 - (temp[0]%10)     # gerando digito verificador    
-    return temp[2]
+        temp[2] = 10 - (temp[0]%10)     # gerando digito verificador
+        if(temp[2] == 10):
+            return 0
+        return temp[2]
 #---------------------------------------------------------------------------------------------------#
 
 # função valida_codigo recebe o codigo de barras com codigo verificador
@@ -55,25 +56,35 @@ def trata_linha(linha):
     for i in range(0,len(linha)-1):
         string = string + linha[i]
     return string
+#---------------------------------------------------------------------------------------------------#
+#lendo arquivo com codigos de barra
+def gera_relatorio(nome):
+    temp = ''
+    cadeia = ''
+    verificador = ''
+    arquivo = open(nome,'r')
+    arquivo2 = open('codigostratados.txt','w')
+    linha_tratada = []
+    codigos = arquivo.readlines()
+    for linha in codigos:
+        temp = trata_linha(linha)
+        if valida_codigo(temp) == 1:
+            verificador = 'verdadeiro'
+        else:
+            verificador = 'falso'
+        cadeia = temp+'\t'+verificador+'\n'
+        arquivo2.write(cadeia)
+    arquivo2.close()
+    arquivo.close()
+    return 1
 #---------------------------------------------------------------------------------------------------#    
-"""temp = ''
-arquivo = open('codigosdebarras.txt','r')
-linha_tratada = []
-codigos = arquivo.readlines()
-for linha in codigos:
-    temp = trata_linha(linha)    
-    print(temp,'\t',valida_codigo(temp))
-arquivo.close()"""
-#---------------------------------------------------------------------------------------------------#    
-
 # programa principal
 
-"""print(valida_codigo('484848498418'))
-print(valida_codigo('1234567890128'))
-print(valida_codigo('15487948191526'))"""
-
-
-validador = gerar_validador('123456789012')
-print('codigo 1234567890128 validador ',validador)
-
-#---------------------------------------------------------------------------------------------------#
+endereco = input('Informe o endereço do arquivo com os codigos de barra\n')
+if(gera_relatorio(endereco)):
+    print('Arquivo codigostradasdos.txt gerado com sucesso.')
+else:
+    print('Ocorreu um erro ao gerar o relatorio')
+ 
+#Por hoje é só pepessoal :b
+#---------------------------------------------------------------------------------------------------#    
