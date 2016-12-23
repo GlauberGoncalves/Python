e538b9633620cd2d1f9701b796a401b320fc9daa
Glauber <glauber_g3@yahoo.com.br>
2016-12-23 22:05:02 +0000

    Uso de tuplas simples

---
+++ 10-tuplas1.py
@@ -0,0 +1,9 @@
+# As tuplas são elementos parecidos com as listas
+# Só que são imutaveis
+# para declarar uma tupla basta por parenteses e
+# os valores 
+
+tupla = (1,2,3,4,'glauber')
+
+print(tupla)
+print(tupla)
