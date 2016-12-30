0801bb168524c283c55030c0d5c50f2e47153e9f
Glauber Gonçalves <glauber_g3@yahoo.com.br>
2016-12-30 00:39:33 -0200

    Trabalhando com a biblioteca tkinter

---
+++ 13-tkinter.py
@@ -0,0 +1,12 @@
+#coding: utf8
+
+from tkinter import *
+
+janela = Tk()
+
+janela.title("Primeiro Programa") # titulo da janela
+janela["bg"] = "#2196f3" # cor de fundo da janela
+janela.geometry("500x300+400+200")
+
+
+janela.mainloop()
